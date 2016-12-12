8d302d0163bc7f266d9b5508b444db6e8c45265e
Jonathan Schultz <jonathan@imatix.com>
2016-12-12 18:01:34 +1100

    Added language column

--- Exporter.py
+++ Exporter.py
@@ -7,7 +7,7 @@ def main(argv):
 	if len(argv) == 0:
 		print 'You must pass some parameters. Use \"-h\" to help.'
 		return
-		
+
 	if len(argv) == 1 and argv[0] == '-h':
 		print """\nTo use this jar, you can pass the folowing attributes:
     username: Username of a specific twitter account (without @)
@@ -25,50 +25,50 @@ def main(argv):
 
  # Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']
  python Exporter.py --username "barackobama" --since 2015-09-10 --until 2015-09-12 --maxtweets 1\n
- 
+
  # Example 4 - Get the last 10 top tweets by username
  python Exporter.py --username "barackobama" --maxtweets 10 --toptweets\n"""
 		return
- 
+
 	try:
 		opts, args = getopt.getopt(argv, "", ("username=", "since=", "until=", "querysearch=", "toptweets", "maxtweets="))
-		
+
 		tweetCriteria = got.manager.TweetCriteria()
-		
+
 		for opt,arg in opts:
 			if opt == '--username':
 				tweetCriteria.username = arg
-				
+
 			elif opt == '--since':
 				tweetCriteria.since = arg
-				
+
 			elif opt == '--until':
 				tweetCriteria.until = arg
-				
+
 			elif opt == '--querysearch':
 				tweetCriteria.querySearch = arg
-				
+
 			elif opt == '--toptweets':
 				tweetCriteria.topTweets = True
-				
+
 			elif opt == '--maxtweets':
 				tweetCriteria.maxTweets = int(arg)
-				
-		
+
+
 		outputFile = codecs.open("output_got.csv", "w+", "utf-8")
-		
-		outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
-		
+
+		outputFile.write('username;date;retweets;favorites;text;lang;geo;mentions;hashtags;id;permalink')
+
 		print 'Searching...\n'
-		
+
 		def receiveBuffer(tweets):
 			for t in tweets:
-				outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
+				outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;%s;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.lang, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
 			outputFile.flush();
 			print 'More %d saved on file...\n' % len(tweets)
-		
+
 		got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
-		
+
 	except arg:
 		print 'Arguments parser error, try -h' + arg
 	finally:
